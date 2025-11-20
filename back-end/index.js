6bfc77bdb414c75ad536fdb765ada2db219b87b6
Gustavo Medeiros Schmeller <gustavo.medsch@gmail.com>
2025-11-20 17:16:00 -0300

    #

--- back-end/index.js
+++ back-end/index.js
@@ -14,15 +14,38 @@ app.use(cors({
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
+
+async function validarPessoa (req,res,next){ 
+    try{
+
+        const { id } = req.params 
+
+        const { data:selectUsuario,error:selectErro } = await supabase
+        .from('tarefas')
+        .select()
+        .eq('id',id)
+
+        if (!selectUsuario || selectUsuario.length === 0) {
+            return res.status(500).json({message: "O usuário não existe"})
+        }
+        if (selectErro) {
+            return res.status(500).json({message: "Houve um erro:", erro: selectErro.message})
+        }
+        next()
+
+    } catch (err){
+        return res.status(500).json({message: "Ocorreu um erro inesperado: ", erro: err.message})
+    }
+}
+
 app.post("/tarefas", async (req,res)=>{
     const { descricao } = req.body;
 
-    const { data,error } = await supabase 
+    const { error } = await supabase 
     .from('tarefas')
     .insert({ 
         descricao:descricao
     })
-    .select()
 
     if (error){
         console.log(error)
@@ -41,22 +64,26 @@ app.get("/tarefas", async(req,res)=>{
     return res.status(200).json({tarefas:data})
 })
 
-app.delete("/tarefas/:id", async (req,res)=>{
+
+
+app.delete("/tarefas/:id", validarPessoa, async (req,res)=>{
     const { id } = req.params;
-    if (!id  || Object.keys(id ).length === 0) return res.status(500).json({message: "Nenhum ID recebido."})
+    if (!id  || id.length === 0) return res.status(500).json({message: "Nenhum ID recebido."})
 
     const { error } = await supabase
-    .from('lista')
+    .from('tarefas')
     .delete()
     .eq('id', id)
 
     if (error){
         console.log(error)
-        return res.status(500).json({message:"Erro ao excluir coluna"})
+        return res.status(500).json({message:"Erro ao excluir usuário"})
     }
     
-    return res.status(200).json({message:id})
+    return res.status(200).json({message:"Usuário excluido com sucesso!"})
 })
+
+app.put()
 
 app.use((req,res,next)=>{
     res.status(404).json({
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,13 @@ import { useEffect, useState } from "react";
 import './output.css'
 
 export default function App() {
+  return <Home/>
+}
+
+//function Login(){}
+//function Logout(){}
+
+function Home(){
   
   const [atualizar, setAtualizar] = useState(0)
   const [texto, setTexto] = useState("")
@@ -9,25 +16,30 @@ export default function App() {
   const [erro, setErro] = useState(false)
   const [carregando, setCarregando] = useState(true)
 
+  // Texto do input
   function atualizarTexto(evento){
     setTexto(evento.target.value)
   }
 
+  // Usado para renderizar a lista no loading da página
+  // É atualizado quando o usuário envia uma nova tarefa
   useEffect(() => {
     fetch("http://localhost:8000/tarefas")
     .then(resposta => resposta.json())
     .then(respostaJson => {
-      setLista(respostaJson.tarefas,
+      setLista(respostaJson.tarefas || []),
       setCarregando(false)
-      )})
+      })
     .catch(err => {
       setErro(true),
       setCarregando(false)
     })
   }, [atualizar]);
 
-  function atualizarLista(){
+  // Criar uma nova tarefa
+  function adicionarTarefa(){
     if (texto.trim() == "") return
+    setLista([... lista, {descricao:"Adicionando... '"+texto+"''"}])
 
     fetch("http://localhost:8000/tarefas", {
       method:"POST",
@@ -37,14 +49,40 @@ export default function App() {
       body: JSON.stringify({descricao:texto})
 
     })
-
-    setLista([... lista, {descricao:texto+" - adicionando..."}])
-    setAtualizar(a => a + 1)
-    setTexto("")
+    .then(respostaServidor => {
+      if (!respostaServidor.ok) throw new Error("Erro ao adicionar nova tarefa")
+        return respostaServidor.json()
+    })
+    .then(respostaStatus => {
+      console.log(respostaStatus.message),
+      setAtualizar(a => a + 1),
+      setTexto("")
+    })
+    .catch(err => {
+      console.log(err.message),
+      alert("Não foi possível adicionar a tarefa")
+    })
   }
 
-  console.log(lista)
+  function apagarTarefa(id){
+    fetch(`http://localhost:8000/tarefas/${id}`, {
+      method: "DELETE",
+    })
+    .then(respostaServidor => {
+      if (!respostaServidor.ok) throw new Error("Erro ao apagar tarefa")
+        return respostaServidor.json()
+    })
+    .then(respostaServidor => {
+      console.log(respostaServidor.message),
+      setAtualizar(a => a + 1)
+    })
+    .catch(err => {
+      console.log(err.message),
+      alert(err.message)
+    })
+  }
   
+  // Tela de loading
   if(carregando){
     return(
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 p-6">
@@ -52,6 +90,7 @@ export default function App() {
     </div>
     )}
 
+  // Tela de erro
   if(erro){
     return(
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 p-6">
@@ -59,16 +98,29 @@ export default function App() {
     </div>
     )}
 
+  // Tela da HOME
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-4 p-6">
-      <form className="flex flex-col items-center gap-2" onSubmit={(e) => {e.preventDefault(),atualizarLista()}}>
+      <span>LISTA DE TAREFAS</span>
 
-        {lista.map((item,index) => 
-          <h2 key={index}>{item.descricao}</h2>
-        )}
+      <form className="flex flex-col items-center gap-2" onSubmit={(e) => {e.preventDefault(),adicionarTarefa()}}>
+        
+        {lista.length === 0 ? (
+            <span>Não há tarefas cadastradas</span>
+        ): (
+          lista.length > 0 && lista.map((item) => (
+          <div key={item.id}>
+            <button type="button" onClick={() => apagarTarefa(item.id)}>{ item.descricao } </button>
+            <button type="button" onClick={() => {
+              const antigoTexto = item.descricao
+              const novoTexto = window.prompt("Edite sua tarefa.",antigoTexto)
+            }}>✏️</button>
+          </div>
+        )))}
+
+      
         <input className="border border-gray-400 rounded p-2 w-64 text-center" value={texto} onChange={atualizarTexto}/>
-        <button type="submit" onClick={atualizarLista}>adicionar</button>
-
+        <button type="submit"/>
       </form>
     </div>
   );
